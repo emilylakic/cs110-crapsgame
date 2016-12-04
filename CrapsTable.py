77e08c40505a1835295f6a11d0cf7561cd2aaaa2
Jordana Simon <jsimon7@binghamton.edu>
2016-12-04 18:02:51 -0500

    Add files via upload

---
+++ CrapsTable.py
@@ -0,0 +1,46 @@
+from tkinter import *
+from RollingDie import *
+from import App *
+
+class CrapsTable(Frame):
+    def __init__(self, clock):
+        self.die1 = RollingDie()
+        self.die2=RollingDie()
+        self.delay = 20
+        self.clock = App()
+        self.game = CrapsGame()
+        self.display = DisplayPanel()
+
+    def CrapsTable(display):
+        root = Tk()
+        root.configure(background='green')
+        widget.config(highlightbackground='Orange')
+
+    def rollDie(self):
+        RollingDie.setBounds(-3, 3, 3, -3)
+        self.die1.roll()
+        self.die2.roll()
+        self.clock.start()
+    def actionPerformed():
+        if(isDiceRolling()):
+            if(!self.clock.isRunning()):
+                self.clock.restart()
+            if(self.die1.isRolling()):
+                self.die1.avoidCollision(die2)
+            elif(self.die2.isRolling()):
+                self.die2.avoidCollision(die1)
+            else:
+                self.clock.stop()
+                total = die1.getNumDots() + die2.getNumDots()
+                result = game.processRoll(total)
+                point = game.getPoint()
+                display.update(result, point)
+        repaint()
+
+    def isDiceRolling(self):
+        return self.die1.isRolling() or self.die2.isRolling()
+
+    #def paintComponent():
+    #    super.paintComponent()
+    #    die1.draw(g);
+    #    die2.draw(g);
