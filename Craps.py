5ab60e3deb7462c492147a457f136f46f6948147
Emily Lakic <elakic1@binghamton.edu>
2016-12-07 11:31:04 -0500

    Newest changes updated on Git

--- Craps.py
+++ Craps.py
@@ -2,20 +2,24 @@ from tkinter import *
 from DisplayPanel import *
 from CrapsTable import *
 from ControlPanel import *
-from CrapsGame import *
 
 class Craps(Frame):
     def __init__(self, parent):
-        Frame.__init__(self, parent, background="darkgreen", highlightbackground='Dark Orange', highlightthickness = 10)
+        Frame.__init__(self, parent, background="Dark Green", highlightbackground='Dark Orange', highlightthickness = 10)
         self.parent = parent
         self.parent.title("Craps Game")
         self.pack(fill=BOTH, expand=True)
+
+        self.canvas = Canvas(self,background="Dark Green")
+        self.canvas.pack(fill=BOTH,expand=True,padx=10,pady=10)
+
         self.display = DisplayPanel(self.parent)
-        self.table = CrapsTable(self.parent, self.display)
+        self.table = CrapsTable(self.parent, self.display,self.canvas)
         self.control = ControlPanel(self.table,self.parent)
+
         self.centerWindow()
 
-    
+
     def centerWindow(self):
         w = 750
         h = 550
@@ -27,10 +31,11 @@ class Craps(Frame):
         y = (sh - h)/2
         self.parent.geometry('%dx%d+%d+%d' % (w, h, x, y))
 
-    #Opens score window when game is exited
+    #Opens score window when main game window is exited
     def score(self):
-        self.display.scoreWindow()
         self.parent.destroy()
+        self.display.scoreWindow()
+
 
 def main():
 
