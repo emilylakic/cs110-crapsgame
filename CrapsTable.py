4fab43f67b670c88876613ffe2d0fae054b030d4
Emily Lakic <elakic1@binghamton.edu>
2016-12-06 20:47:34 -0500

    Update CrapsTable.py

--- CrapsTable.py
+++ CrapsTable.py
@@ -4,39 +4,46 @@ from RollingDie import *
 from CrapsGame import *
 
 class CrapsTable(Frame):
-    def __init__(self, parent, display):
-        self.die1 = RollingDie()
-        self.die2 = RollingDie()
+    def __init__(self, parent, display, canvas):
+        self.die1 = RollingDie(canvas)
+        self.die2 = RollingDie(canvas)
         self.delay = 20
         self.display = display
-        #self.clock = App()
         self.game = CrapsGame()
+        self.die1.draw(); 
+        self.die2.draw();
+        
+    def CrapsTable(display): 
+        root = Tk()
+        root.configure(background = 'Dark Green')
+        widget.config(highlightbackground = 'Orange')
         
     # Rolls the dice (called when the "Roll" button is clicked)
     def rollDie(self):
-        print("Rolling Die.....")
-        self.die1.setBounds(-3, 3, 3, -3)
-        self.die2.setBounds(-3, 3, 3, -3)
+        #print("Rolling Die.....") - used to debug previously
+        self.die1.setBounds(25, 675, 25, 445) #changed previous boundaries
+        self.die2.setBounds(25, 675, 25, 445)
         self.die1.roll()
         self.die2.roll()
-        #self.clock.start()
+        #insert clear canvas
+        self.die1.clearCanvas()
+        self.die2.clearCanvas()
+        self.die1.draw()
+        self.die2.draw()
+        self.actionPerformed()
         
     # Processes timer events
-    def actionPerformed():
-        if(isDiceRolling()):
-            #if(not self.clock.isRunning()):
-             #   self.clock.restart()
+    def actionPerformed(self):
+        while(self.isDiceRolling()):
             if(self.die1.isRolling()):
                 self.die1.avoidCollision(self.die2)
             elif(self.die2.isRolling()):
                 self.die2.avoidCollision(self.die1)
-            else:
-              #  self.clock.stop()
-                total = self.die1.getNumDots() + self.die2.getNumDots()
-                result = self.game.processRoll(total)
-                point = self.game.getPoint()
-                self.display.update(result, point)
-        widget.repaint()
+        total = self.die1.getNumDots() + self.die2.getNumDots()
+        result = self.game.processRoll(total)
+        point = self.game.getPoint()
+        self.display.update(result, point)
+        #widget.repaint()
         
     # returns true if dice are still rolling; otherwise
     # returns false
@@ -44,5 +51,5 @@ class CrapsTable(Frame):
         return self.die1.isRolling() or self.die2.isRolling()
 
     def paintComponent():
-        die1.draw(g);
-        die2.draw(g);
+        self.die1.draw(); #previously used g's only for graphics in Java
+        self.die2.draw();
